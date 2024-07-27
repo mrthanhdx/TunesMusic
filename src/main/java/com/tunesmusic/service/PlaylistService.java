7f4f83d35231068c9d1ee35a92fcd3d137fde709
thanhtony2004 <thanhtony2k4dx@gmail.com>
2024-07-27 15:48:41 +0700

    add playlist repo and service

---
+++ src/main/java/com/tunesmusic/service/PlaylistService.java
@@ -0,0 +1,6 @@
+package com.tunesmusic.service;
+
+import com.tunesmusic.model.Playlist;
+
+public interface PlaylistService extends CommonService<Playlist> {
+}
---
+++ src/main/java/com/tunesmusic/service/serviceImpl/PlaylistServiceImpl.java
@@ -0,0 +1,33 @@
+package com.tunesmusic.service.serviceImpl;
+
+import com.tunesmusic.model.Playlist;
+import com.tunesmusic.repository.PlaylistRepository;
+import com.tunesmusic.service.PlaylistService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class PlaylistServiceImpl implements PlaylistService {
+    @Autowired
+    PlaylistRepository playlistRepository;
+    @Override
+    public void save(Playlist playlist) {
+        playlistRepository.save(playlist);
+    }
+
+    @Override
+    public Playlist findById(Long id) {
+        return playlistRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        playlistRepository.deleteById(id);
+    }
+
+    @Override
+    public List<Playlist> findAll() {
+        return playlistRepository.findAll();
+    }
+}
